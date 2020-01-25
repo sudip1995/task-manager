889c26828358ca94c16885059d7043ebeac1f213
sdpsarker88 <sdpsarker88@gmail.com>
2020-01-25 13:11:57 +0600

    added angular material with browser animation and not hammerjs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {TaskManagerModule} from '../modules/task-manager/task-manager.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TaskManagerModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
