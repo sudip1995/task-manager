1549f32825869c0c670bee93efbf71d81e1a0c14
sdpsarker88 <sdpsarker88@gmail.com>
2020-02-13 22:41:28 +0600

    board detail with id graphql call

--- src/modules/task-manager/components/board/board.component.ts
+++ src/modules/task-manager/components/board/board.component.ts
@@ -1,26 +1,75 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
 import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
 import {FormControl} from '@angular/forms';
+import {Apollo} from 'apollo-angular';
+import gql from 'graphql-tag';
+import {ActivatedRoute} from '@angular/router';
+import {takeUntil} from 'rxjs/operators';
+import {Subject} from 'rxjs';
 
 @Component({
   selector: 'app-board',
   templateUrl: './board.component.html',
   styleUrls: ['./board.component.scss']
 })
-export class BoardComponent implements OnInit {
+export class BoardComponent implements OnInit, OnDestroy {
 
-  @Input() boardDetail: any;
+  boardDetail: any;
   listIds: string[];
   listName: FormControl;
-  constructor() { }
+  unSubscribe$ = new Subject();
+  constructor(private apollo: Apollo,
+              private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
-    this.listIds = this.boardDetail.lists.map(l => `${l.id}`);
+    this.activatedRoute.params.pipe(takeUntil(this.unSubscribe$)).subscribe(param => {
+      if (param && param.id) {
+        this.loadBoardDetail(param.id);
+      }
+    });
     this.listName = new FormControl();
   }
+  private loadBoardDetail(boardId: any) {
+    this.apollo.watchQuery<any>({
+      query: gql`
+        query ($boardId: String!) {
+          board(id: $boardId)
+          {
+            id
+            title
+            columns {
+              id
+              boardId
+              title
+              order
+              tickets {
+                id
+                title
+                columnId
+                description
+                order
+              }
+            }
+          }
+        }
+      `,
+      variables: {
+        boardId
+      }
+    }).valueChanges.subscribe(res => {
+        this.boardDetail = res.data.board;
+        this.listIds = this.boardDetail.columns.map(l => `${l.id}`);
+      }
+    );
+  }
 
-  drop(event: CdkDragDrop<any[]>) {
-    moveItemInArray(this.boardDetail.lists, event.previousIndex, event.currentIndex);
-    this.listIds = this.boardDetail.lists.map(l => `${l.id}`);
+  drop(event: CdkDragDrop<string, any>) {
+    moveItemInArray(this.boardDetail.columns, event.previousIndex, event.currentIndex);
+    this.listIds = this.boardDetail.columns.map(l => `${l.id}`);
+  }
+
+  ngOnDestroy(): void {
+    this.unSubscribe$.next();
+    this.unSubscribe$.complete();
   }
 }
--- src/modules/task-manager/components/list/list.component.ts
+++ src/modules/task-manager/components/list/list.component.ts
@@ -14,7 +14,6 @@ export class ListComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    console.log(this.listDetail);
   }
 
   drop(event: CdkDragDrop<any[]>) {
